a214c97424e8ec8d4a0d1db764b0a2dd906a1657
Toushiat Shaikh <shaikhtoushiat96@gmail.com>
2020-02-09 19:03:24 +0600

    PHP File

---
+++ program.php
@@ -0,0 +1,116 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Programms & Events</title>
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+		<link rel="stylesheet" href="style.css">
+	</head>
+	<body>
+	
+		<div class="library">
+			<div class="container hadder">
+				<div class="row">
+					<div class="col-md-3 logo">
+						<img src="image/logo.png" alt="logo" />
+					</div>
+					<div class="col-md-4 col-md-offset-2">
+						<h2>Library Opening Hour</h2>
+						<h4>Saturday-Tusday: 9:00am-9:00pm</h4>
+						<h4>Friday: 10:00am-5:00pm</h4>
+					</div>
+					<div class="col-md-3 src">
+						<div class="form-group">
+							<label for="">SEARCH HARE</label>
+							<div class="input-group">
+								<input type="text" placeholder="Search Here" class="form-control" />
+								<span class="input-group-addon">
+								<i class="glyphicon glyphicon-search"></i>
+								</span>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="container">
+				<div class="row">
+					<ul class="dropdown">
+						
+						<li><a href="#">About Us</a>
+							<ul>
+								<li><a href="mission.php">Library Mission</a></li>
+							
+								<li><a href="program.php">Programe & Event</a></li>
+							</ul>
+						</li>
+						<li><a href="collection.php">Collection</a></li>
+						
+						<li><a href="lockers.php">Lockers</a></li>
+								
+								
+								
+						<li><a href="get_help.php">Get Help</a></li>
+						<li><a href="index.php">LogOut</a>
+							
+						
+						</li>
+						
+						
+
+					</ul>
+					
+				</div>
+			</div>
+			<div class="program">
+				<h1>Programms & Events</h1>
+			</div>
+			<div class="event">
+				<h1>DIU Library Photo Gallery</h1>
+				<div class="container gallery1">
+					<div class="row">
+						<div class="col-md-2 pic1">
+							<img src="image/prg1.jpg">
+						</div>
+						<div class="col-md-2 pic2">
+							<img src="image/prg2.jpg">
+						</div>
+						<div class="col-md-2 pic3">
+							<img src="image/prg3.gif">
+						</div>
+					</div>
+				</div>
+				<div class="container gallery2">
+					<div class="row">
+						<div class="col-md-2 pic4">
+							<img src="image/prg4.jpg">
+						</div>
+						<div class="col-md-2 pic5">
+							<img src="image/prg5.jpg">
+						</div>
+						<div class="col-md-2 pic6">
+							<img src="image/prg6.jpg">
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="container fmenu">
+				<div class="row fmenu1">
+					<ul>
+						
+						<li><a href="#"></a></li>
+						<li><a href="#"></a></li>
+						<li><a href="#"></a></li>
+						<li><a href="#"></a></li>
+						<li><a href="#"></a></li>
+						<li><a href="#"></a></li>
+						
+						
+						
+						
+					</ul>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
+			
