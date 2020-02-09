daec64b309030c687d7b17248b53c6f1b2e00c25
Toushiat Shaikh <shaikhtoushiat96@gmail.com>
2020-02-09 18:57:18 +0600

    PHP File

---
+++ lockers.php
@@ -0,0 +1,101 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Lockers</title>
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+		<link rel="stylesheet" href="style.css">
+	</head>
+	<body>
+	
+		<div class="library">
+			<div class="container hadder">
+				<div class="row">
+					<div class="col-md-3 logo">
+						<img src="image/logo.png" alt="logo" />
+					</div>
+					<div class="col-md-4 col-md-offset-2">
+						<h2>Library Opening Hour</h2>
+						<h4>Saturday-Tusday: 9:00am-9:00pm</h4>
+						<h4>Friday: 10:00am-5:00pm</h4>
+					</div>
+					<div class="col-md-3 src">
+						<div class="form-group">
+							<label for="">SEARCH HARE</label>
+							<div class="input-group">
+								<input type="text" placeholder="Search Here" class="form-control" />
+								<span class="input-group-addon">
+								<i class="glyphicon glyphicon-search"></i>
+								</span>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="container">
+				<div class="row">
+					<ul class="dropdown">
+						
+						<li><a href="#">About Us</a>
+							<ul>
+								<li><a href="mission.php">Library Mission</a></li>
+								
+								<li><a href="program.php">Programe & Event</a></li>
+							</ul>
+						</li>
+						<li><a href="collection.php">Collection</a></li>
+						
+						<li><a href="lockers.php">Lockers</a></li>
+								
+								
+								
+						<li><a href="get_help.php">Get Help</a></li>
+						<li><a href="index.php">LogOut</a>
+							
+						
+						</li>
+					</ul>
+				</div>
+			</div>
+			<div class="locker">
+				<h2>Lockers</h2>
+				<img src="image/locker.jpg">
+				<p>We are pleased to inform that a user friendly and fully accessed controlled locker facility initially with 48 locker units is situated in 1st floor of the DIU Central Library Building.</p>
+				<h3>Lockers are issued on a daily basis until closing time.</h3>
+				<h2>How to obtain a locker</h2>
+				<p>Locker keys are available from the Security Desk in the 1st floor of library building. To get a key you will need your Library card. Only one key and one locker will be allocated per person.</p>
+				<h2>Using your locker</h2>
+					<li>Library lockers are available on a first-come, first served basis and are intended for short-term daily use.</li>
+					<li>No food, beverage, valuable items, dangerous goods or illegal substance may be kept in lockers.</li>
+					<li>The library is not responsible for the loss or damage of any items stored in the locker.</li>
+					<li>Lockers may only be used during library hours. Any belongings left in the lockers at closing time will be removed.</li>
+					<li>The library reserves the right to open the locker at any time.</li>
+					<li>The lockers are to be handled with care and protected from damage and soiling.</li>
+					<li>Security problems and concerns must be reported promptly to Library staff.</li>	
+				<h2>Returning your locker key</h2>
+				<p>You should return the key to the Security Desk.</p>
+				<h2>Lost, stolen, or non-returned keys</h2>
+				<p>The Library must be notified as soon as possible if you lose or have a key stolen.</p>
+				<p>Users are responsible for keeping the key. If the key were lost, a replacement charge will be levied.</p>
+				<p>We welcome your feedback on this new service !!</p>
+			</div>
+			<div class="container fmenu">
+				<div class="row fmenu1">
+					<ul>
+						
+						<li><a href="#"></a></li>
+						<li><a href="#"></a></li>
+						<li><a href="#"></a></li>
+						<li><a href="#"></a></li>
+						<li><a href="#"></a></li>
+						<li><a href="#"></a></li>
+						
+						
+						
+					</ul>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
+			
