9d9d484207d431a8279f78b5613b4c881d21e001
Toushiat Shaikh <shaikhtoushiat96@gmail.com>
2020-02-09 18:42:42 +0600

    PHP file

---
+++ collection.php
@@ -0,0 +1,109 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>LIBRARY COLLECTION</title>
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+		<link rel="stylesheet" href="style.css">
+	</head>
+	<body>
+	
+		<div class="library">
+			<div class="container hadder">
+				<div class="row">
+					<div class="col-md-3 logo">
+						<img src="image/logo.png" alt="logo" />
+					</div>
+					<div class="col-md-4 col-md-offset-2">
+						<h2>Library Opening Hour</h2>
+						<h4>Saturday-Tusday: 9:00am-9:00pm</h4>
+						<h4>Friday: 10:00am-5:00pm</h4>
+					</div>
+					<div class="col-md-3 src">
+						<div class="form-group">
+							<label for="">SEARCH HARE</label>
+							<div class="input-group">
+								<input type="text" placeholder="Search Here" class="form-control" />
+								<span class="input-group-addon">
+								<i class="glyphicon glyphicon-search"></i>
+								</span>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="container">
+				<div class="row">
+					<ul class="dropdown">
+						
+						<li><a href="#">About Us</a>
+							<ul>
+								<li><a href="mission.php">Library Mission</a></li>
+								
+								
+								<li><a href="program.php">Programe & Event</a></li>
+							</ul>
+						</li>
+						<li><a href="collection.php">Collection</a></li>
+						
+						<li><a href="lockers.php">Lockers</a></li>
+							
+						
+						<li><a href="get_help.php">Get Help</a></li>
+						<li><a href="index.php">LogOut</a>
+							
+						
+						</li>
+					</ul>
+				</div>
+			</div>
+			<div class="collection">
+				<h1>LIBRARY COLLECTION</h1>
+				<p>The DIU library collections are a true collaborative effort. They are the foundation upon which a student's education is built and form the cornerstone of DIU research efforts. The library collections include published materials physically held in the DIU Central Library, including the Ashulia Campus, Uttara Campus, and electronic resources which are available online.</p>
+				<h3>GENERAL COLLECTION: </h3>
+				<p>The general collection consists of text books, conference proceedings, reports and others that support and meet the curriculum, research, learning and teaching needs of DIU students, faculty and staffs. Currently over 24,000 volumes of books in different discipline are housed in publicly accessible open stacks. Books are arranged by Dewey decimal classification call number on the library’s  4th floors. Most items in general collection circulate under regular loan periods. We add more than 1,000 new items every year, including multiple copies of key textbooks in many subjects.</p>
+				<h3>REFERENCE: </h3>
+				<p>The DIU Library has a print reference collection.  The reference collection contains various sources of information, such as encyclopedias, atlases, handbooks, yearbooks, almanacs, directories, subject specialist dictionaries, English dictionaries, thesaurus and other specialist resources. This also includes government documents, publications of Bangladesh Bureau Statistics, and reports, documents of national and international organizations. Reference materials are located on 2nd floor of the central library. Items from reference section cannot be borrowed but may be viewed in the library.</p>
+				<h3>PERIODICALS:</h3>
+				<p>DIU library’s periodicals collection located on 4th floor of central library consists of a variety of scholarly journals, popular magazines, periodicals and newspapers. The library subscribes more than 100 periodicals in print. Items from Periodical collection can be borrowed on short loan period.</p>
+				<p>This collection also contains news paper’s magazines, leaflets, loose issues of popular magazines and academic brochures. These are arranged by title in folders. These are non circulating items, but may be viewed in periodical section.</p>
+				<h3>ELECTRONIC RESOURCES</h3>
+				<p>Recognizing the ongoing need to position itself for the digital future, DIU library continued efforts to enrich its Electronic Resource. Through the E-library services, it provides access to an ever widening range of digital library and services, including e-book, full-text online databases, electronic journals articles, E-Magazines, faculty research, dissertations and thesis papers. 
+				At present, DIU library is subscribing to journal database covering almost 25000 electronic journals from 31 renowned vendors. These databases are accessible through library webpage and DIU network only.
+				The library has rapidly growing collection of electronic books. It is also providing access to around 26000 e-books, 322 e magazines. Users have both on and off campus access to e-book, e-magazine with user Id and password authentication.</p>
+				<h3>AUDIOVISIOUAL MATERIALS:</h3>
+				<p>We have a growing collection of over 1900 audio visual materials in 3rd floor of the Central library building. The collection includes computer programs, books on CDs, kit. Audio visual items are available for loan to library users according to their borrowing privileges.</p>
+				<h3>FICTION:</h3>
+				<p>DIU library offers a small fiction collection located on 2nd floor of library building to support the intellectual and creative thoughts of its users. This collection includes popular Bengali and English novels, drama, poetry and story collections for the general adult audience. Fiction items can be borrowed and searched though library online catalog.</p>
+				<h3>ENTREPRENEURSHIP AND SMALL BUSINESS COLLECTION </h3>
+				<p>Entrepreneurship and small business collection is located at the 4th floor. It contains books, reports and materials related to entrepreneurship and small business.</p>
+				<h3>DIU THESIS/ RESEARCH PAPER:</h3>
+				<p>DIU Library collects and preserves thesis paper, project reports written by the final year students of DIU from across the discipline.  
+				Beginning in 2012, Thesis paper, project reports have been collected in CD format. This collection now contains over 1214 titles dating from 2012 to present. The index of this collection is available at the circulation desk on 3rd floor of central library. 
+				Additionally, paper copies of all pre -2012 DIU graduate student theses and project papers are shelved by project number on the 3rd floor of central library building. You can search for paper copies of theses in the Library catalog</p>
+				<h3>NEWSPAPERS:</h3>
+				<p>The DIU library maintains collections of daily print newspapers from the major English and Bengali national newspapers. The most recent issues of these newspapers are displayed on the newspaper stands in 1st floor of central library. We retain back issue of selected significant titles in bound volume format from dating 2oo2.
+				Online access is available to National newspapers.</p>
+			</div>
+			
+			<div class="container fmenu">
+				<div class="row fmenu1">
+					<ul>
+						
+						<li><a href="#"></a></li>
+						<li><a href="#"></a></li>
+						<li><a href="#"></a></li>
+						<li><a href="#"></a></li>
+						<li><a href="#"></a></li>
+						<li><a href="#"></a></li>
+						
+					
+						
+						
+					</ul>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
+			
