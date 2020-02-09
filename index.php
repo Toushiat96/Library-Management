53f42953fea61a1ec6b0d7dc8b99364bd642fc20
Toushiat Shaikh <shaikhtoushiat96@gmail.com>
2020-02-09 18:51:55 +0600

    PHP File

---
+++ index.php
@@ -0,0 +1,133 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+	<head>
+		<meta charset="UTF-8">
+		<title>DIU Library</title>
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+		<link rel="stylesheet" href="style.css">
+	</head>
+	<body>
+		<div class="library">
+			<div class="container hadder">
+				<div class="row">
+					<div class="col-md-3 logo">
+						<img src="image/logo.png" alt="logo" />
+					</div>
+					<div class="col-md-4 col-md-offset-2">
+						<h2>Library Opening Hour</h2>
+						<h4>Saturday-Tusday: 9:00am-9:00pm</h4>
+						<h4>Friday: 10:00am-5:00pm</h4>
+					</div>
+					<div class="col-md-3 src">
+						<div class="form-group">
+							<label for="">SEARCH HARE</label>
+							<div class="input-group">
+								<input type="text" placeholder="Search Here" class="form-control" />
+								<span class="input-group-addon">
+								<i class="glyphicon glyphicon-search"></i>
+								</span>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="container">
+				<div class="row">
+					<ul class="dropdown">
+						<li><a href="index.php">Home</a></li>
+						
+						<li><a href="#">Sing Up / Sing In </a>
+							<ul>
+								<li><a href="sign_in.php">Login</a></li>
+							
+								<li><a href="sign_up.php">Sign Up</a></li>
+								
+							</ul>
+						
+						</li>
+					</ul>
+				</div>
+			</div>
+			<div class="slider banner">
+			      <img src="image/Slider1.jpg">
+				
+                </div>
+
+					
+			<center>
+			<div class="container item">
+				<div class="row well">
+				
+					<div class="col-md-12 col-md-offset-1 item2">
+						
+						<p>---------------------------------------------------------------------------------------</p>
+						<a href="https://www.youtube.com/watch?v=uV4afWz6W2s"class="btn btn-primary">Library documentation</a>
+							<p>------------------------------------------------------------------------------------------</p>
+					</div>
+					
+				</div>
+			
+			</div>
+		</center>
+		
+			<div class="container ebook">
+				<div class="row">
+					<img src="image/ebook.jpg" alt="book" />
+				</div>
+			</div>
+			<div class="container book">
+				<div class="row">
+					<div class="col-md-6 col-md-offset-2">
+						<img src="image/book.gif" alt="book" />
+					</div>
+				
+				</div>
+			</div>
+			<div class="container link">
+				<div class="row">
+					<div class="col-md-4 link1">
+						<h3>Useful Link</h3>
+						<ul>
+							<li><a href="http://library.daffodilvarsity.edu.bd/en/english-learning">English Learning Center</a></li>
+							<li><a href="http://www.adb.org/documents/series/adb-annual-reports">Asian Development Bank Resources</a></li>
+							<li><a href="http://www.jica.go.jp/english/publications/reports/annual/index.html">Japan International Cooperation Agency</a></li>
+							<li><a href="https://wdronline.worldbank.org/">World Bank Resources</a></li>
+							<li><a href="http://www.gatesfoundation.org/Who-We-Are/Resources-and-Media/Annual-Reports">Bill & Melinda Gates Foundation</a></li>
+						</ul>
+					</div>
+					<div class="col-md-4 link2">
+						<h3>Ext. Registered Members</h3>
+						<img src="image/mbr1.gif" alt="Member" />
+						<img src="image/mbr2.gif" alt="Member" />
+					</div>
+					<div class="col-md-4 link3">
+						<h3>Contributing Resources</h3>
+						<li><a href="http://asiafoundation.org/">The Asia Foundation</a></li>
+						<li><a href="http://www.britishcouncil.org/bangladesh.htm">British Council Library</a></li>
+						<li><a href="http://www.bids-bd.org/">BIDS</a></li>
+						<li><a href="http://www.bbs.gov.bd/">Bangladesh Bureau of Statistics (BBS)</a></li>
+						<li><a href="http://www.bangladesh-bank.org/">Bangladesh Bank</a></li>
+					</div>
+				</div>
+			</div>
+			<div class="container fmenu">
+				<div class="row fmenu1">
+					<ul>
+						
+						<li><a href="#"></a></li>
+						<li><a href="#"></a></li>
+						<li><a href="#"></a></li>
+						<li><a href="#"></a></li>
+						<li><a href="#"></a></li>
+						<li><a href="#"></a></li>
+						
+						
+						
+					</ul>
+				</div>
+			</div>
+		</div>
+		<script> src="js/bootstrap.min.js" type="text/javascript"</script>
+		<script> src="js/jquery-2.2.3.min.js" type="text/javascript"</script>
+	</body>
+</html>
