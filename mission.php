3983f1e9be925896a2f0d25ba51bc712f287af6c
Toushiat Shaikh <shaikhtoushiat96@gmail.com>
2020-02-09 18:58:38 +0600

    PHP File

---
+++ mission.php
@@ -0,0 +1,91 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Misiion and Vission</title>
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+		<link rel="stylesheet" href="style.css">
+	</head>
+	<body>
+	
+		<div class="library">
+			<div class="container hadder">
+				<div class="row">
+					<div class="col-md-3 logo">
+						<img src="image/logo.png" alt="logo" />
+					</div>
+					<div class="col-md-4 col-md-offset-2">
+						<h2>Library Opening Hour</h2>
+						<h4>Saturday-Tusday: 9:00am-9:00pm</h4>
+						<h4>Friday: 10:00am-5:00pm</h4>
+					</div>
+					<div class="col-md-3 src">
+						<div class="form-group">
+							<label for="">SEARCH HARE</label>
+							<div class="input-group">
+								<input type="text" placeholder="Search Here" class="form-control" />
+								<span class="input-group-addon">
+								<i class="glyphicon glyphicon-search"></i>
+								</span>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="container">
+				<div class="row">
+					<ul class="dropdown">
+						
+						<li><a href="#">About Us</a>
+							<ul>
+								<li><a href="mission.php">Library Mission</a></li>
+								
+								<li><a href="program.php">Programe & Event</a></li>
+							</ul>
+						</li>
+						<li><a href="collection.php">Collection</a></li>
+						
+						<li><a href="lockers.php">Lockers</a></li>
+						
+								
+								
+								
+						<li><a href="get_help.php">Get Help</a></li>
+						<li><a href="index.php">LogOut</a>
+							
+						
+						</li>
+					</ul>
+						
+				</div>
+			</div>
+			<div class="container mission">
+				<div class="row">
+				   <h1>Mission of our Library</h1>
+				   <p>1.The purpose of the DIU library is to support academic and research activities. The library carries out its mission by:</p>
+				   <p>2.Fulfilling the course related needs of undergraduates</p>
+				   <p>3.Meeting the scholarly needs of graduate students and faculty members</p>
+				   <p>5.Providing users with access to materials located inside the library system</p>
+				   <p>6.Equipping users with the skills and resources needed for continuous lifelong learning</p>
+				</div>
+			</div>
+			<div class="container fmenu">
+				<div class="row fmenu1">
+					<ul>
+					
+						<li><a href="#"></a></li>
+						<li><a href="#"></a></li>
+						<li><a href="#"></a></li>
+						<li><a href="#"></a></li>
+						<li><a href="#"></a></li>
+						<li><a href="#"></a></li>
+						
+						
+						
+					</ul>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
+			
