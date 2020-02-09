4259374693f42309205da191d925fa080725cb16
Toushiat Shaikh <shaikhtoushiat96@gmail.com>
2020-02-09 19:04:53 +0600

    PHP File

---
+++ support.php
@@ -0,0 +1,86 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Support</title>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+	<h3>login successful</h3>
+	
+		<div class="library">
+			<div class="container hadder">
+				<div class="row">
+					<div class="col-md-3 logo">
+						<img src="image/logo.png" alt="logo" />
+					</div>
+					<div class="col-md-4 col-md-offset-2">
+						<h2>Library Opening Hour</h2>
+						<h4>Saturday-Tusday: 9:00am-9:00pm</h4>
+						<h4>Friday: 10:00am-5:00pm</h4>
+					</div>
+					<div class="col-md-3 src">
+						<div class="form-group">
+							<label for="">SEARCH HARE</label>
+							<div class="input-group">
+								<input type="text" placeholder="Search Here" class="form-control" />
+								<span class="input-group-addon">
+								<i class="glyphicon glyphicon-search"></i>
+								</span>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<center>
+			<div class="container">
+				<div class="row">
+					<ul class="dropdown">
+						
+						
+						<li><a href="#">About Us</a>
+							<ul>
+								<li><a href="mission.php">Library Mission</a></li>
+								
+								<li><a href="program.php">Programe & Event</a></li>
+							</ul>
+						</li>
+						<li><a href="collection.php">Collection</a></li>
+						
+								<li><a href="lockers.php">Lockers</a></li>
+							
+						<li><a href="get_help.php">Get Help</a></li>
+
+						
+					
+					</ul>
+					<br>
+					<br>
+					<br>
+					<ul>
+					<li><a href="index.php">LOGOUT</a></li>
+				    </ul>
+				</div>
+			</div>
+       </center>
+	       
+			<div class="container fmenu">
+				<div class="row fmenu1">
+					<ul>
+						
+						<li><a href="#"></a></li>
+						<li><a href="#"></a></li>
+						<li><a href="#"></a></li>
+						<li><a href="#"></a></li>
+						<li><a href="#"></a></li>
+						<li><a href="#"></a></li>
+					
+					</ul>
+				</div>
+			</div>
+		</div>
+</body>
+</html>
+			
